73aea320ee20a7b208a028055b52d317d1eac28d
xiallyspider <33478703+xiallyspider@users.noreply.github.com>
2018-04-16 11:54:42 +0800

    function

--- main/main.js
+++ main/main.js
@@ -1,4 +1,14 @@
-module.exports = function main() {
+var getCount = function(stringArray){
+    return [{type:'',count: 0}]
+}
+var getName = function(objectArray){
+    return [{name:'', count: 0}]
+}
+var getFinalPrice = function(objectArray){
+    return [{name:'', count:0, price:0.00}]
+}
+module.exports = function main(inputs) {
+    
     console.log("Debug Info");
     return 'Hello World!';
 };
